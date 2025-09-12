ad7c602680a64ba2f43215d3ce99790a509e081e
Oliver Bennett <oliver.bennett@betfair.com.au>
2025-09-13 06:35:58 +1000

    chore: wipe comments and readme

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -2,20 +2,8 @@
 
 namespace PointsBet_Backend_Online_Code_Test
 {
-
-    /*
-    Improve a block of code as you see fit in C#.
-    You may make any improvements you see fit, for example:
-      - Cleaning up code
-      - Removing redundancy
-      - Refactoring / simplifying
-      - Fixing typos
-      - Any other light-weight optimisation
-    */
     public class StringFormatter
     {
-
-        //Code to improve
         public static string ToCommaSepatatedList(string[] items, string quote)
         {
             StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
